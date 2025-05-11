85f74f1839495bcb17eb9fe39af6fd1cb676876d
candycrusher07 <123592519+candycrusher07@users.noreply.github.com>
2025-05-12 00:09:19 +0530

    Update lov.py

--- lov.py
+++ lov.py
@@ -12,7 +12,7 @@ st.markdown(
     f"""
     <style>
     .stApp {{
-        background-color: #fffafc;
+        background-color: #000000;
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
